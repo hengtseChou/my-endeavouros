5b8d8a26925431c3a0bccd0100b699c8bb4ad49b
Hank Chou <htchou@protonmail.com>
2024-05-08 21:56:54 +0800

    install script done

--- functions.sh
+++ functions.sh
@@ -1,29 +1,28 @@
-#!/bin/bash
 _isInstalledPacman() {
-    package="$1";
-    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
-    if [ -n "${check}" ] ; then
-        echo 0; #'0' means 'true' in Bash
-        return; #true
-    fi;
-    echo 1; #'1' means 'false' in Bash
-    return; #false
+    package="$1"
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")"
+    if [ -n "${check}" ]; then
+        echo 0 #'0' means 'true' in Bash
+        return #true
+    fi
+    echo 1 #'1' means 'false' in Bash
+    return #false
 }
 
 _isInstalledYay() {
-    package="$1";
-    check="$(yay -Qs --color always "${package}" | grep "local" | grep "\." | grep "${package} ")";
-    if [ -n "${check}" ] ; then
-        echo 0; #'0' means 'true' in Bash
-        return; #true
-    fi;
-    echo 1; #'1' means 'false' in Bash
-    return; #false
+    package="$1"
+    check="$(yay -Qs --color always "${package}" | grep "local" | grep "\." | grep "${package} ")"
+    if [ -n "${check}" ]; then
+        echo 0 #'0' means 'true' in Bash
+        return #true
+    fi
+    echo 1 #'1' means 'false' in Bash
+    return #false
 }
 
 _isFolderEmpty() {
     folder="$1"
-    if [ -d $folder ] ;then
+    if [ -d $folder ]; then
         if [ -z "$(ls -A $folder)" ]; then
             echo 0
         else
@@ -35,65 +34,65 @@ _isFolderEmpty() {
 }
 
 _installPackagesPacman() {
-    toInstall=();
+    toInstall=()
     for pkg; do
         if [[ $(_isInstalledPacman "${pkg}") == 0 ]]; then
-            echo ":: ${pkg} is already installed.";
-            continue;
-        fi;
-        toInstall+=("${pkg}");
-    done;
+            echo ":: ${pkg} is already installed."
+            continue
+        fi
+        toInstall+=("${pkg}")
+    done
 
-    if [[ "${toInstall[@]}" == "" ]] ; then
+    if [[ "${toInstall[@]}" == "" ]]; then
         # echo "All pacman packages are already installed.";
-        return;
-    fi;
+        return
+    fi
 
     # printf "Package not installed:\n%s\n" "${toInstall[@]}";
-    sudo pacman --noconfirm -S "${toInstall[@]}";
+    sudo pacman --noconfirm -S "${toInstall[@]}"
 }
 
 _installPackagesYay() {
-    toInstall=();
+    toInstall=()
     for pkg; do
         if [[ $(_isInstalledYay "${pkg}") == 0 ]]; then
-            echo ":: ${pkg} is already installed.";
-            continue;
-        fi;
-        toInstall+=("${pkg}");
-    done;
+            echo ":: ${pkg} is already installed."
+            continue
+        fi
+        toInstall+=("${pkg}")
+    done
 
-    if [[ "${toInstall[@]}" == "" ]] ; then
+    if [[ "${toInstall[@]}" == "" ]]; then
         # echo "All packages are already installed.";
-        return;
-    fi;
+        return
+    fi
 
     # printf "AUR packags not installed:\n%s\n" "${toInstall[@]}";
-    yay --noconfirm -S "${toInstall[@]}";
+    yay --noconfirm -S "${toInstall[@]}"
 }
 
 _installSymLink() {
-    symlink="$1";
-    linksource="$2";
-    linktarget="$3";
-    
+    symlink="$1"
+    linksource="$2"
+    linktarget="$3"
+
     if [ -L "${symlink}" ]; then
-    # is a symlink
+        # is a symlink
         rm ${symlink}
-        ln -s ${linksource} ${linktarget} 
+        ln -s ${linksource} ${linktarget}
         echo ":: Symlink ${linksource} -> ${linktarget} created."
     elif [ -d ${symlink} ]; then
-    # is a dir
-        rm -rf ${symlink}/ 
+        # is a dir
+        rm -rf ${symlink}/
         ln -s ${linksource} ${linktarget}
         echo ":: Symlink for directory ${linksource} -> ${linktarget} created."
     elif [ -f ${symlink} ]; then
-    # is a file
-        rm ${symlink} 
-        ln -s ${linksource} ${linktarget} 
+        # is a file
+        rm ${symlink}
+        ln -s ${linksource} ${linktarget}
         echo ":: Symlink to file ${linksource} -> ${linktarget} created."
     else
-        ln -s ${linksource} ${linktarget} 
+        ln -s ${linksource} ${linktarget}
         echo ":: New symlink ${linksource} -> ${linktarget} created."
     fi
 }
@@ -107,30 +106,29 @@ _replaceLineInFile() {
     find_line_counter=0
     line_found=0
 
-    if [ -f $target_file ] ;then
+    if [ -f $target_file ]; then
 
         # Detect Line
-        while read -r line
-        do
+        while read -r line; do
             ((find_line_counter++))
             if [[ $line = *$find_string* ]]; then
                 line_found=$find_line_counter
                 break
-            fi 
-        done < "$target_file"
+            fi
+        done <"$target_file"
 
-        if [[ ! "$line_found" == "0" ]] ;then
-            
+        if [[ ! "$line_found" == "0" ]]; then
+
             #Remove the line
             sed -i "$line_found d" $target_file
 
             # Add the new line
-            sed -i "$line_found i $new_string" $target_file            
+            sed -i "$line_found i $new_string" $target_file
 
         else
             echo "ERROR: cannot find $find_string in $target_file."
             sleep 1
-        fi   
+        fi
 
     else
         echo "ERROR: $target_file not found."
@@ -143,7 +141,7 @@ _addLineToFile() {
     target_file="$2"
 
     if [[ -f $target_file ]]; then
-        echo $new_string >> $target_file
+        echo $new_string >>$target_file
     else
         echo "ERROR: $target_file not found."
         sleep 1
@@ -157,4 +155,26 @@ _copyGSchemasToGLib() {
     schema_dest="$HOME/.local/share/glib-2.0/schemas/"
     mkdir -p $schema_dest
     cp $schema_file $schema_dest
+}
+
+_prompt_proceed() {
+    local userInput
+    local defaultOption="[Y/n]"
+    local defaultReturnValue=0
+    case $2 in
+    -i | --inverse)
+        defaultOption="[y/N]"
+        defaultReturnValue=1
+        ;;
+    esac
+    echo "$1"
+    while true; do
+        read -p "Proceed? $defaultOption " userInput
+        userInput=${userInput:-"${defaultOption:1:1}"} # Default value is either 'y' or 'n' based on the option
+        case $userInput in
+        [yY]*) return $defaultReturnValue ;;
+        [nN]*) return $((1 - defaultReturnValue)) ;;
+        *) echo "Please enter 'y' or 'n'." ;;
+        esac
+    done
 }
--- install.sh
+++ install.sh
@@ -1,56 +1,135 @@
 #!/bin/bash
+# version 2024.05.08
 set -e
 set -u
 set -o pipefail
 
-source ./functions.sh
+local install_gnome=0
+local install_hypr=0
+local install_theming=0
+local install_develop=0
 
+cat <<"EOF"
+ _   _             _    _       _____ ___  ____
+| | | | __ _ _ __ | | _( )___  | ____/ _ \/ ___|
+| |_| |/ _` | '_ \| |/ /// __| |  _|| | | \___ \
+|  _  | (_| | | | |   <  \__ \ | |__| |_| |___) |
+|_| |_|\__,_|_| |_|_|\_\ |___/ |_____\___/|____/
+
+EOF
+
+source ./functions.sh
 sudo reflector --verbose -c TW --protocol https --sort rate --latest 20 --download-timeout 5 --threads 5 --save /etc/pacman.d/mirrorlist
 yay -Syu --noconfirm
 
-# install timeshift and make a fresh snapshot
-source .setup/timeshift.sh
+# install figlet first because i want big words
+_installPackagesPacman figlet
+figlet Timeshift
+if _prompt_proceed "Install Timeshift and make a fresh snapshot."; then
+  source .setup/timeshift.sh
+fi
 
 # install general packages
-source ./packages/utils.sh
-_installPackagesPacman "${packagesPacman[@]}"
-_installPackagesYay "${packagesYay[@]}"
+figlet Utils
+if _prompt_proceed "Install additional utils."; then
+  source ./packages/utils.sh
+  _installPackagesPacman "${packagesPacman[@]}"
+  _installPackagesYay "${packagesYay[@]}"
+fi
 
 # install gnome packages in addition to eos-gnome-pkg-list
-source ./packages/gnome.sh
-_installPackagesPacman "${packagesPacman[@]}"
-_installPackagesYay "${packagesYay[@]}"
-
-# install general applications
-source ./packages/applications.sh
-_installPackagesPacman "${packagesPacman[@]}"
-_installPackagesYay "${packagesYay[@]}"
-source ./setup/fcitx5.sh
-source ./setup/docker.sh
-source ./setup/zsh.sh # after here the shell will be zsh
+figlet GNOME
+if _prompt_proceed "Install additional GNOME packages."; then
+  install_gnome=1
+  source ./packages/gnome.sh
+  _installPackagesPacman "${packagesPacman[@]}"
+  _installPackagesYay "${packagesYay[@]}"
+fi
 
 # install hypr packages
-source ./packages/my-hypr.sh
-_installPackagesPacman "${packagesPacman[@]}"
-_installPackagesYay "${packagesYay[@]}"
-source ./setup/hypr-plugins.sh
+figlet Hyprland
+if _prompt_proceed "Install Hyprland environment."; then
+  install_hypr=1
+  source ./packages/my-hypr.sh
+  _installPackagesPacman "${packagesPacman[@]}"
+  _installPackagesYay "${packagesYay[@]}"
+  source ./setup/hypr-plugins.sh
+fi
+
+# install general applications
+figlet Apps
+if _prompt_proceed "Install useful applications."; then
+  source ./packages/applications.sh
+  _installPackagesPacman "${packagesPacman[@]}"
+  _installPackagesYay "${packagesYay[@]}"
+  source ./setup/fcitx5.sh
+  source ./setup/zsh.sh
+fi
+
+# install development toolkits
+figlet Develop
+if _prompt_proceed "Install development toolkits."; then
+  install_develop=1
+  source ./packages/development.sh
+  _installPackagesPacman "${packagesPacman[@]}"
+  _installPackagesYay "${packagesYay[@]}"
+  source ./setup/rstudio.sh
+  source ./setup/docker.sh
+fi
 
 # install theming packages
-source ./packages/theming.sh
-_installPackagesPacman "${packagesPacman[@]}"
-_installPackagesYay "${packagesYay[@]}"
+figlet Themes
+if _prompt_proceed "Install theming packages."; then
+  install_theming=1
+  source ./packages/theming.sh
+  _installPackagesPacman "${packagesPacman[@]}"
+  _installPackagesYay "${packagesYay[@]}"
+fi
 
 # setup grub theme
-source ./setup/grub.sh
+figlet GRUB
+if _prompt_proceed "Install grub theme."; then
+  source ./setup/grub.sh
+fi
 
 # create config file symlinks
-git clone https://github.com/hank-chouu/my-hypr $HOME
-source ./setup/symlinks.sh
-source $HOME/.zshrc
+if [ "$install_hypr" -eq 1 ] && [ "$install_theming" -eq 1 ]; then
+  figlet my-hypr
+  if _prompt_proceed "Install my Hyprland configs."; then
+    git clone https://github.com/hank-chouu/my-hypr $HOME
+    source ./setup/symlinks.sh
+    source $HOME/.zshrc
+  fi
+fi
 
-source ./setup/git.sh
-source ./setup/npm.sh
-source ./setup/go.sh
-source ./setup/pipx.sh
-source ./setup/gnome-extensions.sh
-source ./setup/gsettings.sh
+# install extensions
+if [ "$install_develop" -eq 1 ]; then
+  figlet Extensions
+  if _prompt_proceed "Install extensions (npm/go/pipx)."; then
+    source ./setup/npm.sh
+    source ./setup/go.sh
+    source ./setup/pipx.sh
+    if [ "$install_gnome" -eq 1] && [ "$install_theming" -eq 1 ]; then
+      source ./setup/gnome-extensions.sh
+      source ./setup/gsettings.sh
+    fi
+  fi
+fi
+
+# install executables
+figlet bin
+if _prompt_proceed "Install my executables."; then
+  git clone https://github.com/hank-chouu/my-bin $HOME/bin
+fi
+
+# git configs
+figlet git
+if _prompt_proceed "Set git configs. Don't proceed unless you are me." -i; then
+  source ./setup/git.sh
+fi
+
+# complete
+figlet Done
+if _prompt_proceed "It is recommended to reboot your system."; then
+  systemctl reboot
+fi
