4d4a668af43579fd078bdf11a5428b29871f8928
HANK CHOU <hengtse.me@gmail.com>
2024-05-10 16:12:15 +0800

    resolve npm global installation permission issue

--- setup/npm.sh
+++ setup/npm.sh
@@ -1,3 +1,6 @@
+mkdir $HOME/.npm-global
+npm config set prefix "$HOME/.npm-global"
+export PATH=$HOME/.npm-global/bin:$PATH
 npm install --global git-open
 npm install --global @marp-team/marp-cli
 npm install --global fkill-cli
