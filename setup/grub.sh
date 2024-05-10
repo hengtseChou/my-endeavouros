3bc95449405a8a25060de8a1705b2f3f7f14a9ae
HANK CHOU <hengtse.me@gmail.com>
2024-05-10 16:02:54 +0800

    modify clone path

--- setup/grub.sh
+++ setup/grub.sh
@@ -1,5 +1,5 @@
 pwd="$PWD"
-git clone https://github.com/vinceliuice/grub2-themes.git $HOME
+git clone https://github.com/vinceliuice/grub2-themes.git $HOME/grub2-theme
 cd $HOME/grub2-theme
 sudo ./install.sh -t vimix -s 1080p -i color
 cd $pwd
--- setup/rstudio.sh
+++ setup/rstudio.sh
@@ -2,5 +2,5 @@ pwd=$PWD
 git clone https://gist.github.com/9bad76d97ff17e37980cf40416fc5596.git $HOME/rstudio-desktop-bin
 cd $HOME/rstudio-desktop-bin
 makepkg
-sudo pacman -U --noconfirm rstudio*
+sudo pacman -U --noconfirm rstudio-desktop-bin-2024.04.0.735-1-x86_64.pkg.tar.zst
 cd $pwd
