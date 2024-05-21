b1fb2ce420925d6d80f4cb4aff353a902aa35213
HANK CHOU <hengtse.me@gmail.com>
2024-05-21 22:37:51 +0800

    add githook dir to setup

--- setup/git.sh
+++ setup/git.sh
@@ -1,3 +1,4 @@
 git config --global user.name "HANK CHOU"
 git config --global user.email "hengtse.me@gmail.com"
-git config --global --add --bool push.autoSetupRemote true
+git config --global push.autoSetupRemote true
+git config --global core.hooksPath .githooks/
