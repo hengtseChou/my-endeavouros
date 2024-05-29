0dc14606761765f7e4ca30016b89acf1b722f6fd
HANK CHOU <hengtse.me@gmail.com>
2024-05-29 16:34:00 +0800

    add more global git config

--- setup/git.sh
+++ setup/git.sh
@@ -2,3 +2,5 @@ git config --global user.name "HANK CHOU"
 git config --global user.email "hengtse.me@gmail.com"
 git config --global push.autoSetupRemote true
 git config --global core.hooksPath .githooks/
+git config --global push.followTags true
+git config --global init.defaultBranch main
